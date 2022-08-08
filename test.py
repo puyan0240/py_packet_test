067f58f12282cbdd921de5deac359f67312108c8
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-08-08 23:26:50 +0900

    実験仮コミット

--- test.py
+++ test.py
@@ -4,15 +4,24 @@ from scapy.all import *
 
 target = '192.168.0.21'
 
-#pkt = IP(dst=target)/ICMP()
+pkt = IP()/UDP()/Raw()
 
-pkt = IP()/UDP()
+#IP Header
 pkt.dst = '192.168.0.21'
-pkt.id = 99
-pkt.show()
+#pkt.id = 99
 
+#UDP Header
+pkt.sport = 40000
 pkt.dport = 555
 
+#User Data
+pkt.load = "1234".encode()
+
+
+#Packet Show Command
+pkt.show()
+
+#Packet Send Command
 send(pkt)
 
 #sr1(pkt)
