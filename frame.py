cff80d5cfdf49e41e8d6a83e6b5679c47f347cef
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-08-10 00:01:51 +0900

    Frame仮

--- frame.py
+++ frame.py
@@ -1,7 +1,20 @@
+from cProfile import label
 import tkinter
 
 root = tkinter.Tk()
 root.title("Packet Test")
 root.geometry("600x400")
+
+#MAIN Frame
+frame_main = tkinter.Frame(root, width=100, bg='dark cyan')
+frame_main.pack(side=tkinter.LEFT)
+
+#Control Frame
+frame_ctrl = tkinter.Frame(root, bg='BLUE')
+frame_ctrl.pack(side=tkinter.RIGHT)
+
+#STATUS Frame
+frame_status = tkinter.Frame(root, bg='RED')
+frame_status.pack(side=tkinter.BOTTOM)
 
 root.mainloop()
