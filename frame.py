c04a6430a64a37e71cf9461bd216622faf7de66a
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-08-11 18:22:14 +0900

    gridに置き換え

--- frame.py
+++ frame.py
@@ -67,37 +67,37 @@ frame_ip.pack(fill=tkinter.X)
 #部品配置--------------------------------------------------
 #タイトル
 label_ip_title  = tkinter.Label(frame_ip, padx=20, text="IP: ", font=('System', 12))
-label_ip_title.pack(fill=tkinter.X)
+label_ip_title.grid(row=0, column=0, columnspan=8, sticky=tkinter.W)
 
 #Version
 label_ip_ver = tkinter.Label(frame_ip, text="version (0-15):")
-label_ip_ver.pack(side=tkinter.LEFT)
+label_ip_ver.grid(row=1, column=0, sticky=tkinter.W)
 text_ip_ver = tkinter.Entry(frame_ip, width=4)
 text_ip_ver.insert(tkinter.END, "4")
-text_ip_ver.pack(side=tkinter.LEFT)
+text_ip_ver.grid(row=1, column=1, sticky=tkinter.W)
 
 #ヘッダ長
 label_ip_ihl = tkinter.Label(frame_ip, text="ヘッダ長 (空白->自動 | 0-15):")
-label_ip_ihl.pack(side=tkinter.LEFT)
+label_ip_ihl.grid(row=1, column=2, sticky=tkinter.W)
 text_ip_ihl = tkinter.Entry(frame_ip, width=4)
-text_ip_ihl.pack(side=tkinter.LEFT)
+text_ip_ihl.grid(row=1, column=3, sticky=tkinter.W)
 
 #サービスタイプ
 label_ip_tos = tkinter.Label(frame_ip, text="ToS (0-127):")
-label_ip_tos.pack(side=tkinter.LEFT)
+label_ip_tos.grid(row=1, column=4, sticky=tkinter.W)
 text_ip_tos = tkinter.Entry(frame_ip, width=4)
 text_ip_tos.insert(tkinter.END, "0")
-text_ip_tos.pack(side=tkinter.LEFT)
+text_ip_tos.grid(row=1, column=5, sticky=tkinter.W)
 
 #全長
 label_ip_tl = tkinter.Label(frame_ip, text="全長 (空白->自動 | 0-65535):")
-label_ip_tl.pack(side=tkinter.LEFT)
+label_ip_tl.grid(row=1, column=6, sticky=tkinter.W)
 text_ip_tl = tkinter.Entry(frame_ip, width=6)
-text_ip_tl.pack(side=tkinter.LEFT)
+text_ip_tl.grid(row=1, column=7, sticky=tkinter.W)
 
 #識別番号
 label_ip_id = tkinter.Label(frame_ip, text="識別番号 (空白->自動 | 0-65535):")
-label_ip_id.pack(side=tkinter.LEFT)
+label_ip_id.grid(row=2, column=0)
 
 
 
