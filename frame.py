fda167b9c3c2c920e95cba1094088ee1c4b5b6d5
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-08-11 18:04:18 +0900

    IP 識別番号までFrame作成

--- frame.py
+++ frame.py
@@ -26,7 +26,7 @@ def btn_top_click():
 
         #ラジオボタン規制解除
         radio_top_auto.config(state=tkinter.NORMAL)
-        radio_top_auto.config(state=tkinter.NORMAL)
+        radio_top_manual.config(state=tkinter.NORMAL)
 
 
 
@@ -70,19 +70,34 @@ label_ip_title  = tkinter.Label(frame_ip, padx=20, text="IP: ", font=('System', 
 label_ip_title.pack(fill=tkinter.X)
 
 #Version
-ip_ver_val = []
-for i in range(16):
-    ip_ver_val.append(str(i))
-
-ip_ver_grp = tkinter.StringVar()
-cbox_ip_ver = ttk.Combobox(frame_ip, textvariable=ip_ver_grp, values=ip_ver_val)
-cbox_ip_ver.pack()
-
-label_ip_ver = tkinter.Label(frame_ip, text="version")
+label_ip_ver = tkinter.Label(frame_ip, text="version (0-15):")
 label_ip_ver.pack(side=tkinter.LEFT)
-
-text_ip_ver = tkinter.Entry(frame_ip, text="4")
+text_ip_ver = tkinter.Entry(frame_ip, width=4)
+text_ip_ver.insert(tkinter.END, "4")
 text_ip_ver.pack(side=tkinter.LEFT)
+
+#ヘッダ長
+label_ip_ihl = tkinter.Label(frame_ip, text="ヘッダ長 (空白->自動 | 0-15):")
+label_ip_ihl.pack(side=tkinter.LEFT)
+text_ip_ihl = tkinter.Entry(frame_ip, width=4)
+text_ip_ihl.pack(side=tkinter.LEFT)
+
+#サービスタイプ
+label_ip_tos = tkinter.Label(frame_ip, text="ToS (0-127):")
+label_ip_tos.pack(side=tkinter.LEFT)
+text_ip_tos = tkinter.Entry(frame_ip, width=4)
+text_ip_tos.insert(tkinter.END, "0")
+text_ip_tos.pack(side=tkinter.LEFT)
+
+#全長
+label_ip_tl = tkinter.Label(frame_ip, text="全長 (空白->自動 | 0-65535):")
+label_ip_tl.pack(side=tkinter.LEFT)
+text_ip_tl = tkinter.Entry(frame_ip, width=6)
+text_ip_tl.pack(side=tkinter.LEFT)
+
+#識別番号
+label_ip_id = tkinter.Label(frame_ip, text="識別番号 (空白->自動 | 0-65535):")
+label_ip_id.pack(side=tkinter.LEFT)
 
 
 
