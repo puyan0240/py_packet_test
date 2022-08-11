ebd7342c6a71dac10d458b0a9f747a89db9bb137
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-08-11 13:19:42 +0900

    TOP部分作成中

--- frame.py
+++ frame.py
@@ -1,20 +1,60 @@
-from cProfile import label
 import tkinter
+
+
+start_flag = False
+
+
+def radio_top_click():
+    print(radio_top_text[radio_top_val.get()])
+
+def btn_top_click():
+    if start_flag == False:
+        btn_top.config(text='停止', bg='RED')
+    else:
+        btn_top.config(text='開始', bg='GREEN')
+    print("aaaa")
+    print(radio_top_text[radio_top_val.get()])
+
+
 
 root = tkinter.Tk()
 root.title("Packet Test")
-root.geometry("600x400")
+root.geometry("800x400")
 
+
+###########################################################
+#TOP Frame
+###########################################################
+frame_top = tkinter.Frame(root,bg='WHITE', relief=tkinter.SOLID, bd=1,width=500)
+frame_top.pack(fill=tkinter.X)
+
+#部品配置
+label_top_mode  = tkinter.Label(frame_top, padx=20, text="動作モード: ", font=('System', 12))
+label_top_mode.pack(side=tkinter.LEFT, anchor=tkinter.W, fill=tkinter.X)
+
+radio_top_text = ['auto', 'manual']
+radio_top_val  = tkinter.IntVar()
+for i in range(len(radio_top_text)):
+    radio_top = tkinter.Radiobutton(frame_top, value=i, variable=radio_top_val, text=radio_top_text[i], font=('System', 12), command=radio_top_click)
+    radio_top.pack(side=tkinter.LEFT, anchor=tkinter.W, padx=5)
+
+btn_top = tkinter.Button(frame_top, text="開始", font=('System', 12), padx=10, bg='GREEN', command=btn_top_click)
+btn_top.pack(side=tkinter.RIGHT, padx=10)
+
+
+
+
+###########################################################
 #MAIN Frame
+###########################################################
 frame_main = tkinter.Frame(root, width=100, bg='dark cyan')
-frame_main.pack(side=tkinter.LEFT)
+frame_main.pack(fill=tkinter.X)
 
-#Control Frame
-frame_ctrl = tkinter.Frame(root, bg='BLUE')
-frame_ctrl.pack(side=tkinter.RIGHT)
 
+###########################################################
 #STATUS Frame
+###########################################################
 frame_status = tkinter.Frame(root, bg='RED')
-frame_status.pack(side=tkinter.BOTTOM)
+frame_status.pack(fill=tkinter.X)
 
 root.mainloop()
