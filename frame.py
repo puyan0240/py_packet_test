345270903355889ee4dc68d3d37fa1b6ab8baa45
puyan0240-2 <matusima0604@zeus.eonet.ne.jp>
2022-08-11 14:06:46 +0900

    TOPボタン制御の実装

--- frame.py
+++ frame.py
@@ -3,17 +3,30 @@ import tkinter
 
 start_flag = False
 
-
+#ラジオボタン押下
 def radio_top_click():
-    print(radio_top_text[radio_top_val.get()])
+    print("ラジオボタン押下:"+str(radio_top_text[radio_top_val.get()]))
 
+
+#開始/停止ボタン押下
 def btn_top_click():
+    global start_flag
+
     if start_flag == False:
+        start_flag = True
         btn_top.config(text='停止', bg='RED')
+
+        #ラジオボタン規制
+        radio_top_auto.config(state=tkinter.DISABLED)
+        radio_top_manual.config(state=tkinter.DISABLED)
     else:
+        start_flag = False
         btn_top.config(text='開始', bg='GREEN')
-    print("aaaa")
-    print(radio_top_text[radio_top_val.get()])
+
+        #ラジオボタン規制解除
+        radio_top_auto.config(state=tkinter.NORMAL)
+        radio_top_auto.config(state=tkinter.NORMAL)
+
 
 
 
@@ -34,9 +47,10 @@ label_top_mode.pack(side=tkinter.LEFT, anchor=tkinter.W, fill=tkinter.X)
 
 radio_top_text = ['auto', 'manual']
 radio_top_val  = tkinter.IntVar()
-for i in range(len(radio_top_text)):
-    radio_top = tkinter.Radiobutton(frame_top, value=i, variable=radio_top_val, text=radio_top_text[i], font=('System', 12), command=radio_top_click)
-    radio_top.pack(side=tkinter.LEFT, anchor=tkinter.W, padx=5)
+radio_top_auto = tkinter.Radiobutton(frame_top, value=0, variable=radio_top_val, text=radio_top_text[0], font=('System', 12), command=radio_top_click)
+radio_top_auto.pack(side=tkinter.LEFT, anchor=tkinter.W, padx=5)
+radio_top_manual = tkinter.Radiobutton(frame_top, value=1, variable=radio_top_val, text=radio_top_text[1], font=('System', 12), command=radio_top_click)
+radio_top_manual.pack(side=tkinter.LEFT, anchor=tkinter.W, padx=5)
 
 btn_top = tkinter.Button(frame_top, text="開始", font=('System', 12), padx=10, bg='GREEN', command=btn_top_click)
 btn_top.pack(side=tkinter.RIGHT, padx=10)
