019052da829cf583e0b361fd1d63a652de4f778d
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-08-15 10:15:35 +0900

    ランダム文字列生成を追加

--- common.py
+++ common.py
@@ -1,8 +1,10 @@
 import random
+import string
 
 
 ############################################################
 # ランダム値生成　重複なし
+#   引数: min=最小値,max=最大値,num=取得するランダム値の個数
 #   戻り値: int型のリスト
 ############################################################
 def random_ints_nodup(min, max, num):
@@ -12,3 +14,13 @@ def random_ints_nodup(min, max, num):
         if not n in ns:
             ns.append(n)
     return ns
+
+
+############################################################
+# ランダム文字列生成
+#   引数: n=文字数
+#   戻り値: str型
+############################################################
+def random_str(n):
+   random_lst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
+   return ''.join(random_lst)
